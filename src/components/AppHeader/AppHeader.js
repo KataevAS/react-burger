d09bd84bb2c3ed0f26292e37a34173235fbc34c9
KataevAS <38090158+KataevAS@users.noreply.github.com>
2021-06-25 16:46:27 +0300

    moving NavButton to a folder

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './AppHeader.module.css';
+import NavButton from '../NavButton';
+
 
 class AppHeader extends React.Component {
   constructor(props) {
@@ -45,37 +47,6 @@ class AppHeader extends React.Component {
       </header>
     )
   }
-}
-
-
-const NavButton = ({ icon, text, url = '#', disable }) => {
-
-  let iconComponent = null;
-
-  switch (icon) {
-    case 'constructor':
-      iconComponent = <BurgerIcon type={disable ? 'secondary' : 'primary'} />
-      break;
-    case 'tape':
-      iconComponent = <ListIcon type={disable ? 'secondary' : 'primary'} />
-      break;
-    case 'personal':
-      iconComponent = <ProfileIcon type={disable ? 'secondary' : 'primary'} />
-      break;
-    default:
-      break;
-  }
-
-  return (
-    <li className={`${styles.navItem} ${disable && 'text_color_inactive'} mt-4 mb-4 `}>
-      <a href={url} className={`${styles.navLink} pl-5 pr-5 pt-4 pb-4`}>
-        {iconComponent}
-        <span className={`ml-2`}>
-          {text}
-        </span>
-      </a>
-    </li>
-  )
 }
 
 export { AppHeader };
