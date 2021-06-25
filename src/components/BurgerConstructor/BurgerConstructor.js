47020f6eccee8e8314fdda640ee2c17ef66d3523
KataevAS <38090158+KataevAS@users.noreply.github.com>
2021-06-25 18:31:17 +0300

    fix ConstructorElement css style

--- src/components/BurgerConstructor/BurgerConstructor.js
+++ src/components/BurgerConstructor/BurgerConstructor.js
@@ -2,13 +2,14 @@ import { Button, ConstructorElement, CurrencyIcon } from '@ya.praktikum/react-de
 import React from 'react'
 import styles from './BurgerConstructor.module.css';
 
+
 class BurgerConstructor extends React.Component {
 
 
   render() {
     return (
       <section className={`${styles.box} ml-10 pl-4 pr-4`}>
-        <div className={`mt-25`} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+        <div className={`mt-25`} style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
           <ConstructorElement
             type="top"
             isLocked={true}
@@ -16,47 +17,37 @@ class BurgerConstructor extends React.Component {
             price={200}
             thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
           />
-          <div className={`${styles.scrollBox} pr-2`} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-            <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
-            <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
-            <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
-            <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
-            <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
-            <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
-            <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
-            <ConstructorElement
-              text="Краторная булка N-200i (верх)"
-              price={50}
-              thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-            />
+          <div className={`${styles.scrollBox} pr-2`}>
+            <ul style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+              <li>
+                <ConstructorElement
+                  text="Биокотлета из марсианской Магнолии"
+                  price={50}
+                  thumbnail={"https://code.s3.yandex.net/react/code/meat-01.png"}
+                />
+              </li>
+              <li>
+                <ConstructorElement
+                  text="Соус Spicy-X"
+                  price={50}
+                  thumbnail={"https://code.s3.yandex.net/react/code/sauce-02.png"}
+                />
+              </li>
+              <li>
+                <ConstructorElement
+                  text="Мясо бессмертных моллюсков Protostomia"
+                  price={50}
+                  thumbnail={"https://code.s3.yandex.net/react/code/meat-02.png"}
+                />
+              </li>
+              <li>
+                <ConstructorElement
+                  text="Соус традиционный галактический"
+                  price={50}
+                  thumbnail={"https://code.s3.yandex.net/react/code/sauce-03.png"}
+                />
+              </li>
+            </ul>
           </div>
           <ConstructorElement
             type="bottom"
@@ -67,7 +58,7 @@ class BurgerConstructor extends React.Component {
           />
         </div>
         <div className={`${styles.totalPrice} mt-10`}>
-          <div className={`mr-10`}>
+          <div className={`${styles.totalPriceBox} mr-10`}>
             <span className={`text text_type_digits-medium mr-2`}>123</span>
             <CurrencyIcon type="primary" />
           </div>
