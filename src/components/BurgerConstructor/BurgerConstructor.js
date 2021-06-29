b190b9ddb6da96bb3646c28ff3a84c648cc13a77
KataevAS <38090158+KataevAS@users.noreply.github.com>
2021-06-29 13:09:54 +0300

    use Modal

--- src/components/BurgerConstructor/BurgerConstructor.js
+++ src/components/BurgerConstructor/BurgerConstructor.js
@@ -1,83 +1,107 @@
 import { Button, ConstructorElement, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './BurgerConstructor.module.css';
 import PropTypes from 'prop-types';
+import ModalOverlay from '../ModalOverlay';
+import Modal from '../Modal';
+import OrderDetails from '../OrderDetails';
 
 
 const BurgerConstructor = (props) => {
 
+  const [order] = useState('034536');
+  const [modalStatus, setModalStatus] = useState(false);
+
+  const onCloseModal = () => {
+    setModalStatus(false);
+  }
+
+  const onOpenModal = () => {
+    setModalStatus(true);
+  }
+
   return (
-    <section className={`${styles.box} ml-10 pl-4 pr-4`}>
-      <div className={`mt-25`} style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-        <ConstructorElement
-          type="top"
-          isLocked={true}
-          text="Краторная булка N-200i (верх)"
-          price={200}
-          thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-        />
-        <div className={`${styles.scrollBox} pr-2`}>
-          <ul style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
-            <li className={`${styles.constructorElem}`}>
-              <div className={`${styles.dragIcon}`}>
-                <DragIcon type="primary" />
-              </div>
-              <ConstructorElement
-                text={props.ingredients[3].name}
-                price={props.ingredients[3].price}
-                thumbnail={props.ingredients[3].image}
-              />
-            </li>
-            <li className={`${styles.constructorElem}`}>
-              <div className={`${styles.dragIcon}`}>
-                <DragIcon type="primary" />
-              </div>
-              <ConstructorElement
-                text={props.ingredients[4].name}
-                price={props.ingredients[4].price}
-                thumbnail={props.ingredients[4].image}
-              />
-            </li>
-            <li className={`${styles.constructorElem}`}>
-              <div className={`${styles.dragIcon}`}>
-                <DragIcon type="primary" />
-              </div>
-              <ConstructorElement
-                text={props.ingredients[5].name}
-                price={props.ingredients[5].price}
-                thumbnail={props.ingredients[5].image}
-              />
-            </li>
-            <li className={`${styles.constructorElem}`}>
-              <div className={`${styles.dragIcon}`}>
-                <DragIcon type="primary" />
-              </div>
-              <ConstructorElement
-                text={props.ingredients[6].name}
-                price={props.ingredients[6].price}
-                thumbnail={props.ingredients[6].image}
-              />
-            </li>
-          </ul>
+    <>
+      {
+        modalStatus &&
+        <ModalOverlay onHandleClose={onCloseModal}>
+          <Modal onHandleClose={onCloseModal}>
+            <OrderDetails order={order} />
+          </Modal>
+        </ModalOverlay>
+      }
+      <section className={`${styles.box} ml-10 pl-4 pr-4`}>
+        <div className={`mt-25`} style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+          <ConstructorElement
+            type="top"
+            isLocked={true}
+            text="Краторная булка N-200i (верх)"
+            price={200}
+            thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
+          />
+          <div className={`${styles.scrollBox} pr-2`}>
+            <ul style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+              <li className={`${styles.constructorElem}`}>
+                <div className={`${styles.dragIcon}`}>
+                  <DragIcon type="primary" />
+                </div>
+                <ConstructorElement
+                  text={props.ingredients[3].name}
+                  price={props.ingredients[3].price}
+                  thumbnail={props.ingredients[3].image}
+                />
+              </li>
+              <li className={`${styles.constructorElem}`}>
+                <div className={`${styles.dragIcon}`}>
+                  <DragIcon type="primary" />
+                </div>
+                <ConstructorElement
+                  text={props.ingredients[4].name}
+                  price={props.ingredients[4].price}
+                  thumbnail={props.ingredients[4].image}
+                />
+              </li>
+              <li className={`${styles.constructorElem}`}>
+                <div className={`${styles.dragIcon}`}>
+                  <DragIcon type="primary" />
+                </div>
+                <ConstructorElement
+                  text={props.ingredients[5].name}
+                  price={props.ingredients[5].price}
+                  thumbnail={props.ingredients[5].image}
+                />
+              </li>
+              <li className={`${styles.constructorElem}`}>
+                <div className={`${styles.dragIcon}`}>
+                  <DragIcon type="primary" />
+                </div>
+                <ConstructorElement
+                  text={props.ingredients[6].name}
+                  price={props.ingredients[6].price}
+                  thumbnail={props.ingredients[6].image}
+                />
+              </li>
+            </ul>
+          </div>
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text="Краторная булка N-200i (низ)"
+            price={200}
+            thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
+          />
         </div>
-        <ConstructorElement
-          type="bottom"
-          isLocked={true}
-          text="Краторная булка N-200i (низ)"
-          price={200}
-          thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-        />
-      </div>
-      <div className={`${styles.totalPrice} mt-10`}>
-        <div className={`${styles.totalPriceBox} mr-10`}>
-          <span className={`text text_type_digits-medium mr-2`}>123</span>
-          <CurrencyIcon type="primary" />
+        <div className={`${styles.totalPrice} mt-10`}>
+          <div className={`${styles.totalPriceBox} mr-10`}>
+            <span className={`text text_type_digits-medium mr-2`}>123</span>
+            <CurrencyIcon type="primary" />
+          </div>
+          <Button type="primary" size="large" onClick={onOpenModal}>
+            Оформить заказ
+          </Button>
         </div>
-        <Button type="primary" size="large">
-          Оформить заказ
-        </Button>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
 
--- src/components/IngredientsCard/IngredientsCard.js
+++ src/components/IngredientsCard/IngredientsCard.js
@@ -1,27 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './IngredientsCard.module.css';
 import PropTypes from 'prop-types';
-
+import ModalOverlay from '../ModalOverlay';
+import Modal from '../Modal';
+import IngredientDetails from '../IngredientDetails';
 
 
 export const IngredientsCard = (props) => {
-  const { name, image, price, __v } = props.ing;
+  const { name, image, price, __v, calories, proteins, carbohydrates, fat } = props.ing;
+
+  const [modalStatus, setModalStatus] = useState(false);
+
+  const onCloseModal = () => {
+    setModalStatus(false);
+  }
+
+  const onHandleClick = () => {
+    setModalStatus(true);
+  }
 
   return (
-    <div className={styles.box} >
-      {__v > 0 && <Counter count={__v} />}
-      <div className={`${styles.ingredientsCard}`}>
-        <img src={image} alt={name} className={`ml-4 mr-4`} />
-        <div className={`${styles.price} mt-1 mb-1`}>
-          <span className={`text text_type_digits-default mr-1`}>{price}</span>
-          <CurrencyIcon type="primary" />
+    <>
+      {
+        modalStatus &&
+        <ModalOverlay onHandleClose={onCloseModal}>
+          <Modal onHandleClose={onCloseModal} title={'Детали ингредиента'}>
+            <IngredientDetails
+              image={image}
+              name={name}
+              calories={calories}
+              proteins={proteins}
+              carbohydrates={carbohydrates}
+              fat={fat} />
+          </Modal>
+        </ModalOverlay>
+      }
+      <div className={styles.box} onClick={onHandleClick}>
+        {__v > 0 && <Counter count={__v} />}
+        <div className={`${styles.ingredientsCard}`}>
+          <img src={image} alt={name} className={`ml-4 mr-4`} />
+          <div className={`${styles.price} mt-1 mb-1`}>
+            <span className={`text text_type_digits-default mr-1`}>{price}</span>
+            <CurrencyIcon type="primary" />
+          </div>
+          <p className={`${styles.cardName} text text_type_main-default`}>
+            {name}
+          </p>
         </div>
-        <p className={`${styles.cardName} text text_type_main-default`}>
-          {name}
-        </p>
       </div>
-    </div>
+    </>
 
   )
 }
