61195529ae1d78d42fa81c60427e439e524c1d9a
KataevAS <38090158+KataevAS@users.noreply.github.com>
2021-07-11 23:13:41 +0300

    remove Error in reduce and transfer reduce outside  component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,45 +8,43 @@ import { IngredientsContext } from '../../services/igredientsContext';
 
 const URL_INGREDIENTS = 'https://norma.nomoreparties.space/api/ingredients';
 
+function reducer(state, action) {
+  switch (action.type) {
+
+    case "setIngredients":
+      const IndexInitialBun = action.ingredients.findIndex(i => i.type === 'bun');
+      return ([
+        ...action.ingredients.slice(0, IndexInitialBun),
+        { ...action.ingredients[IndexInitialBun], counter: 1 },
+        ...action.ingredients.slice(IndexInitialBun + 1, action.ingredients.length)
+      ])
+
+    case "incrementIngredients":
+      return state.map(item => item._id === action.id ? { ...item, counter: item.counter + 1 } : item);
+
+    case "decrementIngredients":
+      return state.map(item => item._id === action.id ? { ...item, counter: item.counter > 0 ? item.counter - 1 : 0 } : item);
+
+    case "changeBun":
+      return state.map(item => {
+        if (item.type === 'bun' && item._id === action.id) {
+          return { ...item, counter: 1 }
+        } else if (item.type === 'bun' && item._id !== action.id) {
+          return { ...item, counter: 0 }
+        } else {
+          return item
+        }
+      })
+
+    default:
+      return state;
+  }
+}
+
 
 export const App = () => {
 
-  function reducer(state, action) {
-    switch (action.type) {
-
-      case "setIngredients":
-        const IndexInitialBun = action.ingredients.findIndex(i => i.type === 'bun');
-        return ([
-          ...action.ingredients.slice(0, IndexInitialBun),
-          { ...action.ingredients[IndexInitialBun], counter: 1 },
-          ...action.ingredients.slice(IndexInitialBun + 1, action.ingredients.length)
-        ])
-
-      case "incrementIngredients":
-        return state.map(item => item._id === action.id ? { ...item, counter: item.counter + 1 } : item);
-
-      case "decrementIngredients":
-        return state.map(item => item._id === action.id ? { ...item, counter: item.counter > 0 ? item.counter - 1 : 0 } : item);
-
-      case "changeBun":
-        return state.map(item => {
-          if (item.type === 'bun' && item._id === action.id) {
-            return { ...item, counter: 1 }
-          } else if (item.type === 'bun' && item._id !== action.id) {
-            return { ...item, counter: 0 }
-          } else {
-            return item
-          }
-        })
-
-      default:
-        throw new Error(`Wrong type of action: ${action.type}`);
-    }
-  }
-
   const [ingredients, setIngredients] = useReducer(reducer, []);
-
-
 
 
   useEffect(() => {
