9312bf04e9dae2be869783bee152e32da260eace
KataevAS <38090158+KataevAS@users.noreply.github.com>
2021-07-14 12:16:52 +0300

    move initial store in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import { compose } from 'redux';
+import thunk from 'redux-thunk';
 
+import { rootReducer } from './services/reducers';
 import AppHeader from '../AppHeader';
 import styles from './App.module.css';
 import BurgerIngredients from '../BurgerIngredients';
 import BurgerConstructor from '../BurgerConstructor';
+
+
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, enhancer);
 
 
 
@@ -14,12 +30,14 @@ export const App = () => {
   return (
     <>
       <AppHeader />
-      <DndProvider backend={HTML5Backend}>
-        <main className={styles.main}>
-          <BurgerIngredients />
-          <BurgerConstructor />
-        </main>
-      </DndProvider>
+      <Provider store={store}>
+        <DndProvider backend={HTML5Backend}>
+          <main className={styles.main}>
+            <BurgerIngredients />
+            <BurgerConstructor />
+          </main>
+        </DndProvider>
+      </Provider>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,29 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import { rootReducer } from './services/reducers';
-import { applyMiddleware, createStore } from 'redux';
-import { compose } from 'redux';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
-
-
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-
-const store = createStore(rootReducer, enhancer);
-
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
