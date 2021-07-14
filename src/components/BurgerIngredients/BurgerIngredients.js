4c1892f04116444d9b4bf5ef4fba40a4c75428c5
KataevAS <38090158+KataevAS@users.noreply.github.com>
2021-07-14 13:56:41 +0300

    add highlighting of tabs

--- src/components/BurgerIngredients/BurgerIngredients.js
+++ src/components/BurgerIngredients/BurgerIngredients.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 
@@ -15,7 +15,34 @@ export const BurgerIngredients = () => {
 
   const [currentTab, setCurrentTab] = useState('bun');
   const [modalStatus, setModalStatus] = useState(false);
+  const refTabs = useRef(null);
+  const refTitleBuns = useRef(null);
+  const refSauceBuns = useRef(null);
+  const refMainsBuns = useRef(null);
 
+  const onScrollIngredients = () => {
+    const { bottom } = refTabs.current.getBoundingClientRect();
+    const { y: bY, height: bH } = refTitleBuns.current.getBoundingClientRect();
+    const { y: sY, height: sH } = refSauceBuns.current.getBoundingClientRect();
+    const { y: mY, height: mH } = refMainsBuns.current.getBoundingClientRect();
+
+    const bRange = Math.abs(bY - bottom - (bH / 2));
+    const sRange = Math.abs(sY - bottom - (sH / 2));
+    const mRange = Math.abs(mY - bottom - (mH / 2));
+
+    if (bRange < Math.min(sRange, mRange) && currentTab !== 'bun') {
+      setCurrentTab('bun');
+      return;
+    }
+    if (sRange < Math.min(bRange, mRange) && currentTab !== 'sauce') {
+      setCurrentTab('sauce');
+      return;
+    }
+    if (mRange < Math.min(sRange, bRange) && currentTab !== 'main') {
+      setCurrentTab('main');
+      return;
+    }
+  }
 
   useEffect(() => {
     dispatch(getIngredients());
@@ -63,7 +90,7 @@ export const BurgerIngredients = () => {
       <section className={styles.box}>
         <h1 className={`text text_type_main-large mt-10 mb-5`}>Соберите бургер</h1>
 
-        <div className={`mb-10`} style={{ display: 'flex' }}>
+        <div className={`mb-10`} style={{ display: 'flex' }} ref={refTabs}>
           <Tab value="bun" active={currentTab === 'bun'} onClick={onHandleClick}>
             Булки
           </Tab>
@@ -75,7 +102,7 @@ export const BurgerIngredients = () => {
           </Tab>
         </div>
 
-        <div className={`${styles.ingredientsBox}`}>
+        <div className={`${styles.ingredientsBox}`} onScroll={onScrollIngredients}>
 
           {
             buns.length === 0 && sauce.length === 0 && mains.length === 0 && <div>Загрузка...</div>
@@ -84,7 +111,7 @@ export const BurgerIngredients = () => {
           {
             buns?.length > 0 &&
             <>
-              <h2 className={`text text_type_main-medium`}>Булки</h2>
+              <h2 className={`text text_type_main-medium`} ref={refTitleBuns}>Булки</h2>
               <article className={`${styles.ingredients} pl-4 pr-4 pt-6 pb-10`}>
                 {buns.map((item, index) => <IngredientsCard
                   type={item.type}
@@ -102,7 +129,7 @@ export const BurgerIngredients = () => {
           {
             sauce?.length > 0 &&
             <>
-              <h2 className={`text text_type_main-medium`}>Соусы</h2>
+              <h2 className={`text text_type_main-medium`} ref={refSauceBuns}>Соусы</h2>
               <article className={`${styles.ingredients} pl-4 pr-4 pt-6 pb-10`}>
                 {sauce.map((item, index) => <IngredientsCard
                   type={item.type}
@@ -120,7 +147,7 @@ export const BurgerIngredients = () => {
           {
             mains?.length > 0 &&
             <>
-              <h2 className={`text text_type_main-medium`}>Начинки</h2>
+              <h2 className={`text text_type_main-medium`} ref={refMainsBuns}>Начинки</h2>
               <article className={`${styles.ingredients} pl-4 pr-4 pt-6 pb-10`}>
                 {mains.map((item, index) => <IngredientsCard
                   type={item.type}
