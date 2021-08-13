e19d8d25f6c271e4e4f8467495b6edb525a18d8a
KataevAS <38090158+KataevAS@users.noreply.github.com>
2021-08-13 23:04:44 +0300

    fix formatDate function

--- src/utils/functions/formatDate.js
+++ src/utils/functions/formatDate.js
@@ -1,12 +1,16 @@
 export const formatDate = (createdAt) => {
+  const addZero = (n) => {
+    return n < 10 ? '0' + n : n
+  }
+
   const createTime = new Date(createdAt)
   const now = new Date()
 
   const lastDay = Math.ceil(Math.abs(now.getTime() - createTime.getTime()) / (1000 * 3600 * 24))
-  const lastTime = createTime.getHours() + ':' + createTime.getMinutes()
+  const lastTime = addZero(createTime.getHours()) + ':' + addZero(createTime.getMinutes())
   const intervalGMT = new Date().getTimezoneOffset() / 60
 
-  return `${lastDay === 1 ? 'Сегоня' : lastDay === 2 ? 'Вчера' : lastDay + ' дней назад'} ${lastTime} i-GMT${
+  return `${lastDay === 1 ? 'Сегоня,' : lastDay === 2 ? 'Вчера,' : lastDay + ' дней назад,'} ${lastTime} i-GMT${
     intervalGMT > 0 ? '-' + intervalGMT : '+' + intervalGMT * -1
   }`
 }
